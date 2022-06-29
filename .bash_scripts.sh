2b46864c03ee277d482e5890d3a0a65af4cf789b
Alex Leventis <alex.leventis@bit.ly>
2022-06-29 14:33:39 -0400

    update

--- .bash_scripts.sh
+++ .bash_scripts.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-function scripts() { code ~/scripts/.bash_scripts.sh }
-
-function restart() { clear ; exec zsh -l }
+alias scripts='code ~/scripts/.bash_scripts.sh'
+alias restart='source ~/.zshrc'
 
 function vm() {
         if ping -c 1 -W 1 "dev.bitly.org" | grep -q "1 packets received"; then
@@ -16,19 +15,7 @@ function startwork() {
         local tab1="github.com/akleventis" tab2="gmail.com" tab3="calendar.google.com/"
         open --new -a "Google Chrome" --args --profile-directory="Profile 1"  $tab1 $tab2 $tab3
         open /Applications/Slack.app ; open /Applications/Visual\ Studio\ Code.app ; 
-        sleep 2 ; test ; vm 
-}
-
-function test() {
-        local jazz="https://www.youtube.com/watch?v=mgBFGwsmdOQ"
-        osascript -e 'tell application "System Events"
-                do shell script quoted form of "/System/Applications/Mission Control.app/Contents/MacOS/Mission Control"
-                click button 1 of group "Spaces Bar" of group 1 of group "Mission Control" of process "Dock"
-                do shell script quoted form of "/System/Applications/Mission Control.app/Contents/MacOS/Mission Control"
-                key code 124 using control down
-                end tell'
-        open --new -a "Google Chrome" --args --profile-directory="Profile 2"  $jazz ; sleep 2 ; 
-        osascript -e 'tell application "System Events" to key code 123 using control down'
+        sleep 2 ; tube "$1" ; vm 
 }
 
 function tube() {
@@ -37,12 +24,19 @@ function tube() {
       "lofi") tab="https://bit.ly/3MSt1tP" ;;
       "jazz") tab="https://bit.ly/3ar8fDd"
     esac
-    open --new -a "Google Chrome" --args --profile-directory="Profile 2" $tab
+        osascript -e 'tell application "System Events"
+                do shell script quoted form of "/System/Applications/Mission Control.app/Contents/MacOS/Mission Control"
+                click button 1 of group "Spaces Bar" of group 1 of group "Mission Control" of process "Dock"
+                do shell script quoted form of "/System/Applications/Mission Control.app/Contents/MacOS/Mission Control"
+                key code 124 using control down
+                end tell'
+        open --new -a "Google Chrome" --args --profile-directory="Profile 2"  $tab ; sleep 2 ; 
+        osascript -e 'tell application "System Events" to key code 123 using control down'
 }
 
 function compress() {
         gifsicle -O3 --lossy=80 $1 -o $1
-        echo "sweg"
+        echo "done"
 }
 
 function gif() {
